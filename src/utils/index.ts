0eb23813bb76f914a897b4bb7bd0b9629903964e
hamid2117 <saim008865@gmail.com>
2025-07-26 22:35:44 +0500

    refactor(utils): migrate utility functions to TypeScript and remove old JavaScript files

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export { default as createHash } from './createHash';
+export * from './jwt';
+export { default as logger } from './logger';
+export * from './responseHandler';
+export { default as sendEmail } from './sendEmail';
+export { default as sendResetPasswordEmail } from './sendResetPasswordEmail';
+export { default as sendVerificationEmail } from './sendVerficationEmail';
