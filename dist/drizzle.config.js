b5cfffc63508aa7c38e6d8fd226c7516bffa1ddb
hamid2117 <saim008865@gmail.com>
2025-07-26 22:34:42 +0500

    feat: add drizzle configuration files and source maps

---
+++ dist/drizzle.config.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const drizzle_kit_1 = require("drizzle-kit");
+exports.default = (0, drizzle_kit_1.defineConfig)({
+    schema: "./src/db/schema/*.ts",
+    out: "./src/db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL,
+    },
+    verbose: true,
+    strict: true,
+    migrations: {
+        table: "drizzle_migrations",
+        schema: "public",
+    },
+});
+//# sourceMappingURL=drizzle.config.js.map
