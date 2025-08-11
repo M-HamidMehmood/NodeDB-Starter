79993024982c2792c2cdaf0e76b94a9f7754ed75
m-hamidmehmood <saim008865@gmail.com>
2025-08-11 22:20:22 +0500

    refactor: update error imports to use named exports for improved clarity and consistency

--- errors/index.ts
+++ errors/index.ts
@@ -1,8 +1,8 @@
-import BadRequestError from './bad-request';
-import CustomAPIError from './custom-api';
-import NotFoundError from './not-found';
-import UnauthenticatedError from './unauthenticated';
-import UnauthorizedError from './unauthorized';
+import { BadRequestError } from './bad-request';
+import { CustomAPIError } from './custom-api';
+import { NotFoundError } from './not-found';
+import { UnauthenticatedError } from './unauthenticated';
+import { UnauthorizedError } from './unauthorized';
 
 export { BadRequestError, CustomAPIError, NotFoundError, UnauthenticatedError, UnauthorizedError };
 
